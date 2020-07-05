cdee15a035a96d69391f1c6b777b400761ac24c2
Nicholas Davies <nicholasjohndavies42@gmail.com>
2020-07-06 02:16:35 +0800

    changed file structure

---
+++ src/employee.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableMaker from './table';
+
+function employee(employees) {
+  return (
+    <div className="employeeTable">
+        <TableMaker />
+    </div>
+  );
+}
+
+export default employee;
